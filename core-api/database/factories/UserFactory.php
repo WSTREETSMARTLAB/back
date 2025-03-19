fba1fa551c5b939db5c2619334ce01931cc14ff2
Dmytro Martynov <dmytro_martynov@icloud.com>
2025-03-19 23:57:36 +0200

    company-user-models-migrations-setup - added Company model, migration

--- core-api/database/factories/UserFactory.php
+++ core-api/database/factories/UserFactory.php
@@ -29,8 +29,9 @@ class UserFactory extends Factory
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
-            'country' => fake()->country(),
+            'location' => fake()->country(),
             'phone' => fake()->phoneNumber(),
+            'company_id' => fake()->company(),
             'remember_token' => Str::random(10),
         ];
     }
