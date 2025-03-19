30d244348eba11e743cc7a9f4dbab5648f9cb0f3
Dmytro Martynov <dmytro_martynov@icloud.com>
2025-03-19 22:55:34 +0200

    company-user-models-migrations-setup - setup User model, migration, factory

--- core-api/database/factories/UserFactory.php
+++ core-api/database/factories/UserFactory.php
@@ -25,9 +25,12 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
+            'username' => fake()->username(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
+            'country' => fake()->country(),
+            'phone' => fake()->phoneNumber(),
             'remember_token' => Str::random(10),
         ];
     }
