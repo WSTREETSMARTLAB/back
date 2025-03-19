3b43165c798b18d4d071e32347d6e00bb36f064f
Dmytro Martynov <dmytro_martynov@icloud.com>
2025-03-19 18:10:44 +0200

    api only mode set up - resources removed , web.php returns message

--- core-api/routes/web.php
+++ core-api/routes/web.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json(['message' => 'API only mode enabled'], 200);
 });
