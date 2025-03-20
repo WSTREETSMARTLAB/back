ab3b69edfb8876bd9191daabb70aaa94e3853bf2
Dmytro Martynov <dmytro_martynov@icloud.com>
2025-03-20 11:39:46 +0200

    company-user-models-migrations-setup - UserFactory.php setup

--- core-api/database/factories/UserFactory.php
+++ core-api/database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Company;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
@@ -31,18 +32,23 @@ class UserFactory extends Factory
             'password' => static::$password ??= Hash::make('password'),
             'location' => fake()->country(),
             'phone' => fake()->phoneNumber(),
-            'company_id' => fake()->company(),
+            'phone_verified_at' => now(),
+            'company_id' => Company::factory(),
+            'role' => 'user',
+            'active' => true,
+            'last_login' => now(),
+            'avatar' => fake()->imageUrl(),
+            'bio' => fake()->text(),
+            'website' => fake()->url,
+            'telegram' => fake()->name,
+            'facebook' => fake()->url,
+            'twitter' => fake()->url,
+            'linkedin' => fake()->url,
+            'settings' => [],
+            'has_subscription' => $this->faker->boolean(30),
+            'subscription_plan' => $this->faker->randomElement(['free', 'basic', 'pro']),
+            'subscription_expires_at' => now()->addDays($this->faker->numberBetween(1, 365)),
             'remember_token' => Str::random(10),
         ];
-    }
-
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
-    public function unverified(): static
-    {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
     }
 }
