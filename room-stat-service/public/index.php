a4f8057f97cab66d914e7bb78436c65e5a84adc5
Dmytro Martynov <dmytro_martynov@icloud.com>
2025-03-27 16:11:34 +0200

    room-stat-sensor-microservice-setup - added Router.php

--- room-stat-service/public/index.php
+++ room-stat-service/public/index.php
@@ -2,3 +2,11 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../bootstrap.php';
+
+use App\Core\Router;
+use Symfony\Component\HttpFoundation\Request;
+
+$request = Request::createFromGlobals();
+$router = new Router();
+$response = $router->dispatch($request);
+$response->send();
---
+++ room-stat-service/src/Core/Router.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Core;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class Router
+{
+    protected array $routes = [];
+    protected string $controllerNamespace = 'App\\Http\\Controllers\\';
+
+    public function __construct()
+    {
+        $this->routes = require __DIR__ . "/../../routes/api.php";
+    }
+
+    public function dispatch(Request $request)
+    {
+        $method = $request->getMethod();
+        $path = $request->getPathInfo();
+
+        foreach ($this->routes as $route) {
+            [$routeMethod, $routePath, $handler] = $route;
+
+            if ($method === $routeMethod && $path === $routePath) {
+                [$controllerName, $methodName] = explode('@', $handler);
+
+                $controllerClass = $this->controllerNamespace . $controllerName;
+
+                $controller = new $controllerClass();
+
+                return $controller->{$methodName}($request);
+            }
+        }
+
+        return new Response('Not Found', Response::HTTP_NOT_FOUND);
+    }
+}
