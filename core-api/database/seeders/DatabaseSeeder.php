bcb17a25317da862f3d69c47eda9f8802b573b39
Dmytro Martynov <dmytro_martynov@icloud.com>
2025-03-29 16:18:27 +0200

    tool-entity-integration - added migration, model, factory, seeder, db migrated - seeded

--- core-api/database/seeders/DatabaseSeeder.php
+++ core-api/database/seeders/DatabaseSeeder.php
@@ -14,7 +14,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserSeeder::class,
-            CompanySeeder::class
+            CompanySeeder::class,
+            ToolSeeder::class,
         ]);
     }
 }
