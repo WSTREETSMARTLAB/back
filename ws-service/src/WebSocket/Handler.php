8310ef3079c00ae57576afd2210b6800c12c44cf
Dmytro Martynov <dmytro_martynov@icloud.com>
2025-04-02 15:36:18 +0300

    websocket-ws-microservice-setup - Handler setup

--- ws-service/src/WebSocket/Handler.php
+++ ws-service/src/WebSocket/Handler.php
@@ -7,6 +7,7 @@ use Ratchet\MessageComponentInterface;
 class Handler implements MessageComponentInterface
 {
     protected \SplObjectStorage $clients;
+    protected array $channels = [];
 
     public function __construct()
     {
@@ -20,12 +21,27 @@ class Handler implements MessageComponentInterface
 
     public function onMessage($from, $msg)
     {
-        /* Получили сообщение */
+        $data = json_decode($msg, true);
+
+        if (isset($data['action']) && $data['action'] === 'subscribe') {
+            $channel = "sensor:{$data['token']}:signal";
+            $this->subscribe($from, $channel);
+            return;
+        }
     }
 
     public function onClose($conn)
     {
         $this->clients->detach($conn);
+
+        foreach ($this->channels as $channel => $clients) {
+            foreach ($clients as $client) {
+                if ($client === $conn) {
+                    $this->channels[$channel]->detach($conn);
+                }
+            }
+            unset($client);
+        }
     }
 
     public function onError($conn, $e)
@@ -35,8 +51,21 @@ class Handler implements MessageComponentInterface
 
     public function broadcast(string $channel, string $message): void
     {
-        foreach ($this->clients as $client) {
+        if (!isset($this->channels[$channel])) {
+            return;
+        }
+
+        foreach ($this->channels[$channel] as $client) {
             $client->send($message);
         }
+    }
+
+    public function subscribe($conn, $channel): void
+    {
+        if (!isset($this->channels[$channel])) {
+            $this->channels[$channel] = new \SplObjectStorage();
+        }
+
+        $this->channels[$channel]->attach($conn);
     }
 }
