30a1ac1ff269e18d331c67508c4b91d4949bb7ee
Dmytro Martynov <dmytro_martynov@icloud.com>
2025-04-08 19:11:48 +0300

    tool-settings-dto-implementation - room-stat microservice dto setup , TransmitProcess.php setup

--- room-stat-service/src/Http/Processes/TransmitProcess.php
+++ room-stat-service/src/Http/Processes/TransmitProcess.php
@@ -2,18 +2,25 @@
 
 namespace App\Http\Processes;
 
+use App\Core\DependencyAccessor;
 use App\DTO\SignalDTO;
+use App\Repositories\ToolRepository;
 
 class TransmitProcess
 {
+    private ToolRepository $toolRepository;
+
     public function __construct(private \Redis $session)
     {
+        $this->toolRepository = new ToolRepository((new DependencyAccessor())->db());
     }
 
     public function handle(SignalDTO $signal)
     {
         $token = $this->session->get('token');
         $tool = json_decode($this->session->get('tool'), true);
+
+        $tool['alarms'] = $this->analyzeSignal($signal);
 
         $payload = [
             'sensor_id'   => $tool['id'],
@@ -24,8 +31,16 @@ class TransmitProcess
             'temperature' => $signal->temperature(),
             'humidity'    => $signal->humidity(),
             'light'       => $signal->light(),
+            'alarms'      => $tool['alarms'],
         ];
 
         $this->session->publish("sensor:{$token}:signal", json_encode($payload));
+    }
+
+    private function analyzeSignal(SignalDTO $signal = null): array
+    {
+        return [
+
+        ];
     }
 }
--- room-stat-service/src/Repositories/ToolRepository.php
+++ room-stat-service/src/Repositories/ToolRepository.php
@@ -10,7 +10,7 @@ class ToolRepository extends Repository
 {
     public function getByToken(string $token): ToolDTO
     {
-        $q = "SELECT id, type, user_id, company_id, name FROM `tools` WHERE code = :code AND is_active = true LIMIT 1";
+        $q = "SELECT id, type, user_id, company_id, name FROM `tools` WHERE code = :code AND active = true LIMIT 1";
         $stmt = $this->db->prepare($q);
         $stmt->bindParam(':code', $token, \PDO::PARAM_STR);
         $stmt->execute();
@@ -21,5 +21,20 @@ class ToolRepository extends Repository
         }
 
         return new ToolDTO($res);
+    }
+
+    public function getToolSettings(int $toolId, int $userId): ToolDTO
+    {
+        $q = "SELECT settings FROM `tools` WHERE id = :id AND user_id = :user_id AND active = true LIMIT 1";
+        $stmt = $this->db->prepare($q);
+        $stmt->bindParam(':id', $toolId, \PDO::PARAM_INT);
+        $stmt->bindParam(':user_id', $userId, \PDO::PARAM_INT);
+        $res = $stmt->fetch(\PDO::FETCH_ASSOC);
+
+        if (!$res) {
+            // todo throw exception
+        }
+
+        return new ToolDTO(json_decode($res, true));
     }
 }
