5659eecf09d0931e92d78017f0f66e6d21e1642a
Dmytro Martynov <dmytro_martynov@icloud.com>
2025-04-09 07:48:03 +0300

    alarm-model-migration-core-service-api - added Enum , migration , model for alarms

---
+++ core-api/database/migrations/2025_04_09_043130_create_alarms_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('alarms', function (Blueprint $table) {
+            $table->id();
+            $table->enum('type', \App\Enums\Alarm::values());
+            $table->foreignId('tool_id')->constrained()->cascadeOnDelete();
+            $table->float('value');
+            $table->timestamp('start');
+            $table->timestamp('end')->nullable();
+            $table->boolean('resolved')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('alarms');
+    }
+};
