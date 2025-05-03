12adfe3eac776ae7799e5bcd935d1c819a4a84dd
Dmytro <107340610+martynovdmytro@users.noreply.github.com>
2025-05-03 15:10:40 +0300

    Test/phpunit test db setup (#19)
    * phpunit-test-db-setup - added test db
    
    * phpunit-test-db-setup - added phpunit.xml to core-api
    
    * phpunit-test-db-setup - test db migrated , seeded
    
    * phpunit-test-db-setup - minor fixes

--- core-api/config/database.php
+++ core-api/config/database.php
@@ -63,6 +63,25 @@ return [
             ]) : [],
         ],
 
+        'mysql_unit_test' => [
+            'driver' => 'mysql',
+            'database' => 'w5smtlab_test',
+            'host' => env('DB_TEST_HOST', '127.0.0.1'),
+            'port' => env('DB_TEST_PORT', '3306'),
+            'username' => env('DB_TEST_USERNAME', 'forge'),
+            'password' => env('DB_TEST_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => false,
+            'engine' => 'InnoDB',
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
