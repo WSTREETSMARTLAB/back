a77c64e82313a4ea5d725e6494b5f8efb449fcdb
martynovdmytro <dmytro_martynov@icloud.com>
2025-05-29 11:02:20 +0300

    alarms-delete-api-endpoint - added base endpoint

--- core-api/app/Http/Controllers/AlarmController.php
+++ core-api/app/Http/Controllers/AlarmController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Processes\Alarm\DeleteAlarmsProcess;
 use App\Http\Processes\Alarm\GetAlarmListProcess;
+use App\Http\Requests\DeleteAlarmsRequest;
 use Illuminate\Http\Response;
 
 class AlarmController extends Controller
@@ -18,5 +20,10 @@ class AlarmController extends Controller
         return response()->json([
             'data' => $data
         ], Response::HTTP_OK);
+    }
+
+    public function delete(int $toolId, DeleteAlarmsRequest $request, DeleteAlarmsProcess $process)
+    {
+        return "ok";
     }
 }
---
+++ core-api/app/Http/Processes/Alarm/DeleteAlarmsProcess.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Http\Processes\Alarm;
+
+class DeleteAlarmsProcess
+{
+
+}
